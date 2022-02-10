0eff061c7383fb345ec658334309bb8235e9e6c8
sequierh <hugo.sequier@etu.umontpellier.fr>
2022-02-10 23:08:49 +0100

    Etudes

--- src/app/mockimage.ts
+++ src/app/mockimage.ts
@@ -3,8 +3,8 @@ import { PathImage } from "./pathImage";
 export const LISTIMAGES:PathImage[] = [
     {imagePath:'../../assets/images/competences.png', desc : 'Mes Compétences',nom:'/competences'},
     {imagePath:'../../assets/images/etudes.png', desc : 'Mes Etudes',nom:'/etudes'},
-    {imagePath:'../../assets/images/projets.png', desc : 'Mes Projets',nom:'/projets'},
     {imagePath:'../../assets/images/experiences.png', desc : 'Mes Expériences',nom:'/experiences'},
+    {imagePath:'../../assets/images/projets.png', desc : 'Mes Projets',nom:'/projets'},
     {imagePath:'../../assets/images/loisirs.png', desc : 'Mes Loisirs',nom:'/loisirs'},
     {imagePath:'../../assets/images/contact.png', desc : 'Me Contacter',nom:'/contact'}
 ];
