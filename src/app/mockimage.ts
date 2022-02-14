4c9db09c132c13f1513c7559a965e72c9a3010bd
Hugo SEQUIER <hugo.sequier@etu.umontpellier.fr>
2022-02-14 13:13:36 +0100

    Enfin ?

--- src/app/mockimage.ts
+++ src/app/mockimage.ts
@@ -1,10 +1,10 @@
 import { PathImage } from "./pathImage";
 
 export const LISTIMAGES:PathImage[] = [
-    {imagePath:'../../assets/images/competences.png', desc : 'Mes Compétences',nom:'/competences'},
-    {imagePath:'../../assets/images/etudes.png', desc : 'Mes Etudes',nom:'/etudes'},
-    {imagePath:'../../assets/images/experiences.png', desc : 'Mes Expériences',nom:'/experiences'},
-    {imagePath:'../../assets/images/projets.png', desc : 'Mes Projets',nom:'/projets'},
-    {imagePath:'../../assets/images/loisirs.png', desc : 'A propos de moi',nom:'/propos'},
-    {imagePath:'../../assets/images/contact.png', desc : 'Me Contacter',nom:'/contact'}
+    {imagePath:'./assets/images/competences.png', desc : 'Mes Compétences',nom:'/competences'},
+    {imagePath:'./assets/images/etudes.png', desc : 'Mes Etudes',nom:'/etudes'},
+    {imagePath:'./assets/images/experiences.png', desc : 'Mes Expériences',nom:'/experiences'},
+    {imagePath:'./assets/images/projets.png', desc : 'Mes Projets',nom:'/projets'},
+    {imagePath:'./assets/images/loisirs.png', desc : 'A propos de moi',nom:'/propos'},
+    {imagePath:'./assets/images/contact.png', desc : 'Me Contacter',nom:'/contact'}
 ];
